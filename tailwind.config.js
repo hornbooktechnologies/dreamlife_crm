b783e50d0421d9d9ff8aa7b0e2919d9ac24dcf26
Mehul <fiveonline.in@gmail.com>
2026-03-10 11:40:20 +0530

    logo and theme color change

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#3a7fc4",
-        "primary-hover": "#2d6aa8",
+        primary: "#D0AF6A",
+        "primary-hover": "#B79352",
       },
       fontFamily: {
         sans: ['"Montserrat"', "sans-serif"],
